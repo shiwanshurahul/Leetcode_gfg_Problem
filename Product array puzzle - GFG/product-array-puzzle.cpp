602a48f7f6c33d7eacd238476254cafb5e18e6d5
Shiwanshu <110823952+shiwanshurahul@users.noreply.github.com>
2023-04-12 20:23:47 +0530

    Added solution - LeetHub

--- Product array puzzle - GFG/product-array-puzzle.cpp
+++ Product array puzzle - GFG/product-array-puzzle.cpp
@@ -14,17 +14,18 @@ class Solution{
     // nums: given vector
     // return the Product vector P that hold product except self at each index
     vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
-       vector<long long int> ans;
-       for(int i=0;i<n;i++){
-           long long prod =1;
-           for(int j=0;j<n;j++){
-               if(i==j)
-               continue;
-               else
-               prod =prod*nums[j];
-           }
-           ans.push_back(prod);
-       }
+       vector<long long int> ans(n),left(n),right(n);
+      left[0] =1;
+      right[n-1] =1;
+      for(int i=1;i<n;i++){
+          left[i] =left[i-1]*nums[i-1];
+      }
+       for(int i=n-2;i>=0;i--){
+          right[i] =right[i+1]*nums[i+1];
+      }
+      for(int i=0;i<n;i++){
+          ans[i] =left[i]*right[i];
+      }
        return ans;
        
        
