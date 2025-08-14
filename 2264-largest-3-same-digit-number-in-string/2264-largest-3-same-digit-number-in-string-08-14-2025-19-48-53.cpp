c9fd09ddfc85c394d719cd1faedc4061c4961451
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2025-08-14 19:48:53 +0530

    Time: 4 ms (5.98%), Space: 10.4 MB (6.2%) - LeetHub

---
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string-08-14-2025-19-48-53.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        int i=0;
+        int j=0;
+        unordered_map<int,int> mp;
+        string ans="";
+        
+        while(j< num.length()){
+            mp[num[j]]++;
+            if(j-i+1 <3){
+                j++;
+                continue;
+            }
+            else if(j-i+1 == 3){  //3 ke aage mt jane do
+           
+                if(mp.size() == 1){
+                    if(!ans.empty() && ans[0] < num[j]){  //777 > 333
+                        ans.clear();
+                    }
+                    ans+= num[j]; ans+= num[j]; ans+= num[j]; 
+                        //remove calc for i
+                    mp[num[i]]--;
+                    if(mp[num[i]] == 0)
+                        mp.erase(num[i]);
+                    i++;
+                }
+                while(mp.size() > 1){ 
+                    mp[num[i]]--;
+                    if(mp[num[i]] == 0)
+                        mp.erase(num[i]);
+                    i++;
+                }
+            }
+            j++;  //har bar
+        }
+        
+        return ans.empty() ? "" : ans.substr(0,3);
+    }
+};
