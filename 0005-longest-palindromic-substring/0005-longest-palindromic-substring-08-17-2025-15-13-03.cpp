b5ea3277511c7a12d3eaf93278aaf7cb576de3cf
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2025-08-17 15:13:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring-08-17-2025-15-13-03.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+bool is_palindrome(string s, int i, int j){
+    while(i<j){
+        if(s[i]!=s[j])
+            return 0;
+            i++;j--;
+    }
+        return 1;
+}
+    string longestPalindrome(string s) {
+       int n =s.length();
+        if(n == 0 || n==1)
+            return s;
+            
+        int maxi = 0;
+        string ans;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                  if(maxi <j-i+1 && is_palindrome(s,i,j)){
+                        maxi = j-i+1;
+                        ans = s.substr(i,maxi);
+                }
+            }
+        }
+        return ans;
+        ans.clear();
+    }
+};
