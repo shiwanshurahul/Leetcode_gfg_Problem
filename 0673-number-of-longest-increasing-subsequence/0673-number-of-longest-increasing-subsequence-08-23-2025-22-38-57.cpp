93d16d9b01e4a90b6a25be4f4c1cf764468eabf1
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2025-08-23 22:38:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence-08-23-2025-22-38-57.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+int solve(vector<int> &nums, int i, int prev){
+    if( i >= nums.size()){
+
+        return 0;
+    }
+        
+    int skip = 0;
+    int take =0;
+
+    if(prev ==-1 || nums[i] > nums[prev] ){
+        take = 1 + solve(nums,i+1,i);
+        skip = solve(nums,i+1,prev);   //dp hai na
+    }
+    else{
+        skip = solve(nums, i+1,prev);
+    }
+
+    return max(skip, take);  //max len de rha lis ka
+}
+int tabulation(vector<int> &nums){
+    int n = nums.size();
+    vector<int> dp(n,1);
+    
+    //dp[i] -> longest lis ending at idx i
+    // if nums[i] > nums[j] tb dp[i] me store dp[j] pe no of lis +1
+
+    int max_lis = 1;   //default
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(nums[i]> nums[j]){
+                dp[i] = max(dp[i], dp[j]+1);
+                max_lis = max(max_lis, dp[i]);
+            }
+        }
+    }
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        if(dp[i] == max_lis-1)
+            cnt++;
+    }
+    return max_lis == 1 ? n : cnt;  
+}
+    int findNumberOfLIS(vector<int>& nums) {
+    //  return solve(nums, 0, -1); 
+     return tabulation(nums);  
+    }
+};
