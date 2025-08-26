75a8f2b54c2f80ce11c2d0e8947ac1936e81e12f
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2025-08-26 09:27:19 +0530

    Time: 0 ms (100%), Space: 29.4 MB (52.12%) - LeetHub

---
+++ 3000-maximum-area-of-longest-diagonal-rectangle/3000-maximum-area-of-longest-diagonal-rectangle-08-26-2025-09-27-19.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
+        //bada diagoonal wale ka area store
+        int i =0;
+        double diagonal_length = 0.0;
+        double max_area = 0;
+
+        while(i < dimensions.size() ){
+            if(sqrt(dimensions[i][0]*dimensions[i][0] + dimensions[i][1]*dimensions[i][1]) >= diagonal_length){
+                if(sqrt(dimensions[i][0]*dimensions[i][0] + dimensions[i][1]*dimensions[i][1]) == diagonal_length){
+                    if(max_area < dimensions[i][0]*dimensions[i][1])  //same diagonal length me largest area store
+                        max_area = dimensions[i][0] * dimensions[i][1];
+                        i++; 
+                        continue;
+                }
+                else{
+                diagonal_length = sqrt(dimensions[i][0]* dimensions[i][0] + dimensions[i][1]* dimensions[i][1]); 
+                max_area = dimensions[i][0]* dimensions[i][1];
+                }    
+            }
+            i++;
+        }
+        return int(max_area);
+    }
+};
