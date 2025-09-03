44f361675980169ba57dd046bf71baa71c0bfe04
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2025-09-03 16:02:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target-09-03-2025-16-02-15.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+int solve(vector<int> &nums, int target, int i){
+    if(target==0)
+        return 0; //1 way mila
+
+    if(i>= nums.size() || target <0)
+        return -1e9; 
+
+    int include = 1 + solve(nums,target-nums[i],i+1);
+    int exclude = solve(nums,target,i+1);
+
+    return max(include, exclude);     //ye toh total no of subsequences/comninations dega tmko longest subsequence ka len chahiye  
+}
+    int lengthOfLongestSubsequence(vector<int>& nums, int target) {
+        //sliding window for substring,subarray -> variable size = 2 bar ans dhundhne ki kosis
+        //idhar substring
+        // int i=0,j=0;
+        // int max_len = -1;
+        // int sum=0;
+
+        // while(j<nums.size()){
+        //     sum += nums[j];
+        //     if(sum<target){
+        //         j++;
+        //     }
+        //     else if(sum == target){
+        //         max_len = max(max_len,j-i+1);
+        //         j++;
+        //     }
+        //     else if(sum >= target){
+        //            while(sum>= target){
+        //                sum -= nums[i];
+        //                i++;
+        //                if(sum==target)
+        //                   max_len = max(max_len, j-i+1);
+        //            }
+                    
+        //             j++;
+        //     }
+        // }
+        // return max_len;
+        int ans = solve(nums,target,0);
+        if(ans <=0)
+            return -1;
+        return ans;    
+           
+        } 
+};
