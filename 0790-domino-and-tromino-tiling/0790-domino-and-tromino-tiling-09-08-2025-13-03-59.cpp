1fce152c5ca336c7efccf1a930d1c4b5aeb0818d
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2025-09-08 13:04:00 +0530

    Time: 0 ms (100%), Space: 9 MB (47.09%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling-09-08-2025-13-03-59.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+int M = 1e9 +7;
+int solve(int n, vector<int>&dp) {
+    if(n==1)
+        return 1;
+    if(n==2)
+        return 2;    
+    if(n==3)
+        return 5;   
+
+    if(dp[n] !=-1)
+        return dp[n];
+
+    return dp[n] = ( (2*solve(n-1, dp) )%M + (solve(n-3, dp))%M )%M;     
+
+}
+    int numTilings(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
