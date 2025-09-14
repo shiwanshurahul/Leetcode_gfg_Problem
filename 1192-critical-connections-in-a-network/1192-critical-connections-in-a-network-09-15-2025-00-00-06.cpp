07ad6c2623faef8918f633bdcc0bc8cff26d19fa
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2025-09-15 00:00:07 +0530

    Time: 324 ms (8.49%), Space: 215.5 MB (5.81%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network-09-15-2025-00-00-06.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+//const 2 = edges hai
+vector<vector<int>> ans;
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        //bridges ins a graph -> tajan's algorithm
+        unordered_map<int, vector<int>> adj;
+        for(auto it: connections){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+
+        int timer = 0;
+        vector<int> disc(n,-1);
+        vector<int> vis(n,0);
+        vector<int> low(n,-1);
+      
+        dfs(0,adj,vis,timer,disc,low,-1);
+
+        return ans;
+    }
+    void dfs(int node, unordered_map<int, vector<int>>& adj, vector<int>&vis, int timer, vector<int>& disc, vector<int>&low, int parent){
+        vis[node] = 1;
+        disc[node] = low[node] = timer++;
+
+        for(auto it: adj[node]){
+            if(it==parent)
+                continue;
+            if(!vis[it]){
+                dfs(it,adj,vis,timer,disc,low,node);
+                low[node] = min(low[node], low[it]);
+                //check edge is bridge or not
+                if(low[it] > disc[node])
+                    ans.push_back({it,node});
+            }    
+            else {  //visited hai it = back edge hai 
+                low[node] = min(low[node], low[it]);
+            }    
+        }
+        return;
+    }
+};
