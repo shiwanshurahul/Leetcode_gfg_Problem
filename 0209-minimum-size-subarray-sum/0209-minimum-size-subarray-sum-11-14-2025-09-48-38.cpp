1643087dbd833922c83d4d93fd2c550e82a78a97
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2025-11-14 09:48:38 +0530

    Time: 0 ms (100%), Space: 32.1 MB (72.07%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum-11-14-2025-09-48-38.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        //variable size window -> 2 bar ans cal krne ki kosis
+        int i=0,j=0;
+        int ans=1e9;
+        int sum=0;
+        while(j<nums.size()){
+            sum+=nums[j];
+            if(sum<target){
+                j++;
+            }
+            else if(sum>=target){
+                while(sum>=target){
+                    ans = min(ans,j-i+1);   //ans store
+                   sum-=nums[i];            // remove caln for i
+                   i++;                     // slide window  
+                }
+                j++;
+            }
+        }
+        if(ans==1e9)
+        return 0;
+        return ans;
+    }
+};
