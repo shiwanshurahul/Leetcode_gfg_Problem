daff1585fbd873bac334ffdc0a9e2676e6b1fc41
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2025-11-23 19:18:52 +0530

    Time: 57 ms (16.73%), Space: 53.9 MB (8.85%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change-11-23-2025-19-18-51.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int solve(vector<int>& coins, int amount, int i, vector<vector<int>>& dp){
+    if(amount==0)
+        return 0;  //1 way mila -> add 0 and return
+
+    if(amount<0 || i>= coins.size())
+        return 1e9;  //min chahiye toh return 1e9
+
+    if(dp[i][amount] !=-1)
+        return dp[i][amount];
+
+    int pick = 1 + solve(coins,amount-coins[i],i, dp);
+    int skip = solve(coins, amount,i+1, dp);
+
+    return dp[i][amount] = min(pick, skip);
+
+}
+    int coinChange(vector<int>& coins, int amount) {
+        vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1,-1));
+        int num = solve(coins,amount,0, dp);
+        return num == 1e9 ? -1 : num; 
+    }
+};
