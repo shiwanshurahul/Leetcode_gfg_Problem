d17549a132f2df4fc589ef56826a3ee7c7100c68
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2025-12-21 14:30:00 +0530

    Time: 265 ms (10.35%), Space: 307.6 MB (5%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph-12-21-2025-14-30-00.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+typedef long long ll;
+void dfs(int node, vector<int>&vis, ll &cnt, vector<vector<int>>& adj){
+    vis[node] = 1;
+    cnt++;
+    for(auto it: adj[node]){
+        if(!vis[it])
+            dfs(it,vis,cnt,adj);
+    }
+}
+void bfs(int node, vector<int>&vis, ll &cnt, vector<vector<int>>& adj){
+    queue<int> q;
+
+    q.push(node);
+    vis[node] = 1;
+    cnt++;
+
+    while(!q.empty()){
+        int curr = q.front();
+        q.pop();
+        for(auto it: adj[curr]){
+            if(!vis[it]){
+                q.push(it);
+                vis[it] = 1;
+                 cnt++;
+            }
+        }
+    }
+}
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        int nodes_remaining = n; //initally n
+        vector<vector<int>> adj(n);
+        for(auto it: edges){
+            adj[it[0]].push_back(it[1]);  
+            adj[it[1]].push_back(it[0]);  //undirected graph
+        }
+        vector<int> vis(n,0);
+
+        ll ans = 0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                ll cnt=0;
+                // dfs(i,vis,cnt,adj);  //pass by reference
+                bfs(i,vis,cnt,adj);
+                ans += cnt * (nodes_remaining - cnt);
+                nodes_remaining = (nodes_remaining - cnt);
+            }
+        }
+        return ans;
+    }
+};
