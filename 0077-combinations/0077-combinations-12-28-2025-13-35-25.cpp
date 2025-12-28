d856f7451e663739dde7f802063524d3ca1cdc17
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2025-12-28 13:35:26 +0530

    Time: 97 ms (17.29%), Space: 108 MB (26.24%) - LeetHub

---
+++ 0077-combinations/0077-combinations-12-28-2025-13-35-25.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+//backtracking -> pick krke explore fir skip krke explore
+vector<vector<int>> ans;
+vector<int> temp;
+void solve(int n,int k, int i){
+    if(k==0){
+        ans.push_back(temp);
+        return;
+    }
+    if(i>n)
+        return;
+
+    temp.push_back(i);
+    solve(n,k-1,i+1); //k-1 bcz i pick krke explore kiya
+    //after recursive call:
+    temp.pop_back();
+    solve(n,k,i+1);  //skip hai i
+}
+void solve1(int n,int k, int start){
+    if(k==0){
+        ans.push_back(temp);
+        return;
+    }
+      if(start>n)
+        return;
+
+    for(int i=start;i<=n;i++){
+        temp.push_back(i);
+        solve(n,k-1,i+1); 
+        temp.pop_back();
+    }    
+}
+
+    vector<vector<int>> combine(int n, int k) {
+        // solve(n,k,1);
+        solve1(n,k,1);  //with for loop
+        return ans;
+    }
+};
