d490467b3a4566b29d99c6e785a7bd24dd3c3969
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2025-12-31 20:11:05 +0530

    Time: 33 ms (55.96%), Space: 30 MB (40.69%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences-12-31-2025-20-11-07.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+vector<vector<int>> ans;
+vector<int> temp;
+int n;
+void solve(vector<int>&nums, int idx){
+    if(temp.size()>=2)
+            ans.push_back(temp);
+    if(idx>=n)
+        return;   //out of bound 
+    unordered_set<int> st;    
+    for(int i=idx;i<n;i++){
+        if( (st.find(nums[i]) == st.end()) && (temp.empty() || nums[i]>=temp.back()) ){
+            temp.push_back(nums[i]);
+            solve(nums,i+1);
+            //after this recursion:
+            temp.pop_back();
+            st.insert(nums[i]);  //use hai curr me -> same as permutn 2
+        }
+    }
+}
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        //all possible combint/permutan -> backtrackin
+        //If choices = many / variable → loop + 1 recursive call
+        //idx+1 tha letter combn wale me
+        n = nums.size(); 
+        solve(nums,0);
+        return ans;
+    } // recursion returns to the most recent function call (top of the call stack), and executes pop_back() there first.
+
+// Why this happens (core concept)
+// When a function returns, execution resumes exactly after the function call line in the same stack frame.
+//simple recursion me v aisa hota
+};
