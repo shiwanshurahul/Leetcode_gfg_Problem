dcfc15aec8ba548727232f94417c2be09e519ef8
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-01-03 15:22:18 +0530

    Time: 276 ms (65.56%), Space: 67.9 MB (37.35%) - LeetHub

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order-01-03-2026-15-22-17.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    #find those id jo Customers me hai but Orders me ni
+    
+    customer_id_did_not_order = np.setdiff1d(customers['id'], orders['customerId'])
+
+    df_ans = pd.DataFrame([])  #ans df me Customers col name chahiy
+    df_ans['Customers'] = customers[ customers['id'].isin(customer_id_did_not_order) ][['name']]
+    return df_ans
