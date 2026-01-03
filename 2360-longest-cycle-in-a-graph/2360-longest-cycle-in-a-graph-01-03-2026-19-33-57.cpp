409e14c7cfba85cd01e4a7a3522b6e589bd21e63
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-01-03 19:33:57 +0530

    Time: 295 ms (18.04%), Space: 208.3 MB (11.44%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph-01-03-2026-19-33-57.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+int ans = -1;
+void dfs(int node, vector<int>&edges, unordered_map<int,int> &dist, vector<int>& vis){
+    vis[node] = 1;
+    int nbr = edges[node];  //-1 or val
+
+    if(nbr != -1 && !vis[nbr]){  //not visited
+        dist[nbr] = dist[node] + 1;
+        dfs(nbr,edges,dist,vis);
+    }
+    else if(nbr != -1 && dist.find(nbr) !=dist.end() ){
+        ans = max(ans, dist[node] -dist[nbr]+1);
+    }
+
+}
+    int longestCycle(vector<int>& edges) {
+        //at most 1 outgoing edge = adj list me max 1 hi aa skta
+        // = no need of adj list 
+        // = a node can max form/participate in 1 cycle only
+        int n = edges.size();
+        vector<int> vis(n,0);
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                 unordered_map<int,int> dist;  //len of cycle
+                dist[i] = 1;
+                dfs(i,edges,dist,vis);
+            }
+        }
+        return ans;
+    }
+};
