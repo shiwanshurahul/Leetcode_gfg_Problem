09d26b6b7a2a25ea38aba3d9d43bc318661892da
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-01-04 13:23:03 +0530

    Time: 288 ms (44.39%), Space: 67.5 MB (31.76%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i-01-04-2026-13-23-03.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    df_ans =  views[views['author_id'] == views['viewer_id']].sort_values(by='author_id')
+    df_ans = df_ans.rename(columns={'author_id' : 'id'}) 
+    return df_ans[['id']].drop_duplicates()
