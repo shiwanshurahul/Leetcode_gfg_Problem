e2dad9630132ec9264896f08c2cd957fdae12793
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-01-04 14:25:30 +0530

    Time: 346 ms (15.08%), Space: 67.9 MB (54.24%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus-01-04-2026-14-25-29.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+
+
+    employees['bonus'] = employees.apply(lambda x : x['salary'] if (x['employee_id'] %2 !=0) & ~(x['name'][0] =='M') else 0 ,axis=1) 
+
+    # or x['name'].str.startswith('M')
+    # axis = 1 applies the function to each row.
+    return employees[['employee_id','bonus']].sort_values(by='employee_id')
+
+    
+
+
