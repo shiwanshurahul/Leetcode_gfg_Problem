cbd27164c6867572dee2b4e893581d9be7902975
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-01-06 12:52:22 +0530

    Time: 46 ms (6.52%), Space: 26.8 MB (7.25%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array-01-06-2026-12-52-21.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+int dp[1000001];
+typedef long long ll;
+const int M = 1e9 +7;
+int solve(int idx, string& s, int n, int k){
+    if(idx>=n)
+        return 1;
+
+    if(s[idx]=='0')
+        return 0;
+
+    if(dp[idx] !=-1)
+        return dp[idx];
+
+    ll ans = 0; 
+    ll num = 0;
+
+    for(int i=idx;i<n;i++){
+        // num = stoi(s.substr(idx,i-idx+1)); -> tle
+        num = num * 10 + (s[i] - '0');
+        if(num>k)
+            break;// No need to check longer segments  
+        ans = ( (ans%M) + (solve(i+1,s,n,k))%M )%M;    
+    }
+    return dp[idx] = (int)ans;    
+}
+    int numberOfArrays(string s, int k) {
+        //har point pe try to split
+        if(s[0]=='0')
+            return 0;
+        memset(dp,-1,sizeof(dp));    
+        return solve(0,s,s.length(),k);
+    }
+};
