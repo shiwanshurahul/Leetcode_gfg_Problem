296f0b3390ef6102c607b42d6b610b1a56bb4382
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-01-17 16:23:19 +0530

    Time: 5 ms (29.63%), Space: 15 MB (33.64%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path-01-17-2026-16-23-19.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        //stack me sirf name (string) store krunga
+        //and wo str ko char se banao str+=path[i]
+        //.. v koi s[i] hoga naki s[i] , s[i+1]
+        // / = // = /// ..
+        // . = curr directory hi hai = continue
+        stack<string> st;
+        int n = path.length();
+        for(int i=0;i< n;i++){
+            string str ="";//har iteratn me str banao
+            if(path[i]== '/'){
+                continue;
+            }
+            while(i<n && path[i] != '/'){ //alpha hai
+                str+=path[i];  // 'h','o','m','e'
+                i++;
+            }
+            if(str ==".."){
+                if(!st.empty())
+                    st.pop();
+            }
+            else if(str =="."){
+                continue; //curr directry hai
+            }
+            else{  //string ko push in st ->home, user
+                st.push(str);  //home
+            }
+        }//end of for loop
+        string ans ="";
+        while(!st.empty()){
+            ans='/'+ st.top()+ans;
+            st.pop();
+        }
+        return ans.size() > 0 ? ans : "/";
+
+    }
+};
