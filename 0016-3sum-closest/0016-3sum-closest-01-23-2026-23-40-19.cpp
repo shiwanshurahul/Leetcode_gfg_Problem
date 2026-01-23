62d3a1d3dc2363c4b662eefdbe2c214269c3f458
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-01-23 23:40:19 +0530

    Time: 15 ms (33.96%), Space: 14.1 MB (28.73%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest-01-23-2026-23-40-19.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+typedef long long ll;
+    int threeSumClosest(vector<int>& nums, int target) {
+        //brute force -> o(n3)
+        //here, o(n^2)logn
+        int n = nums.size();
+        int ans = 1e9;
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<n;i++){
+            int s = i+1, e = n-1;
+            while(s<e){
+                int sum = nums[i] + nums[s] + nums[e];
+                if(abs(sum - target) < abs(ans - target) )
+                    ans = sum;  //closest sum
+
+                if(sum == target)
+                    return sum;    
+                else if( sum> target)
+                    e--;
+                else if(sum < target)
+                    s++;
+            }    
+        }
+        return ans;
+    }
+};
