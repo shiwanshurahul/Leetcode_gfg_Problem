8cd8e3f75051bcc7c217f222190caf82403b71d9
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-01-28 23:13:41 +0530

    Time: 54 ms (5.18%), Space: 114.5 MB (99.45%) - LeetHub

---
+++ 3784-minimum-deletion-cost-to-make-all-characters-equal/3784-minimum-deletion-cost-to-make-all-characters-equal-01-28-2026-23-13-41.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+typedef long long ll;
+    long long minCost(string s, vector<int>& cost) {
+        //min total deletin cost = max deleting cost wale char ko ignor
+        //string me bas ek hi char rkh skte, freq kitni v ho skti
+        //e.g. a ; aaa ; zzzzzz; kk;
+        // = kisi ek hi char ko rkho
+        // = sbko delete krne ka try ek ek krke and min rkhlo
+        ll ans = 1e15;
+        for(auto it='a';it<='z';it++){
+            ll deletion_cost = 0;
+            for(int i=0;i<s.length();i++){
+                if(s[i] != it)  //not same char
+                    deletion_cost += cost[i]; //delete kro 
+            }
+            ans = min(ans,deletion_cost);
+        }
+        return ans;    
+
+
+    }
+};
