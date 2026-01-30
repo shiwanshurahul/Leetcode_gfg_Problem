ad880ff290b93fb8ecee5d67a7c21f3cacc0a444
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-01-30 12:01:11 +0530

    Time: 18 ms (60.19%), Space: 43 MB (76.49%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i-01-30-2026-12-01-12.cpp
@@ -0,0 +1,32 @@
+class MyCalendar {
+public:
+vector<pair<int,int>>v;
+    MyCalendar() {
+        
+    }
+    
+    bool book(int startTime, int endTime) {
+        //[10,20],[20,30]
+        //check for overlapping ->
+        //endTime(prev) > startTime(curr) && endTime(curr) > startTime(prev)
+        //startTime me aa rha
+        for(auto it: v){
+            int s = it.first;
+            int e = it.second;
+            if( e > startTime && s< endTime )
+                return false;   //warna thik hai
+            //m2:
+            if(max(startTime,it.first) < min(endTime,it.second))
+                return false;
+               
+        }
+        v.push_back({startTime,endTime}); 
+        return true;
+    }
+};//general: check overlap for(a1,b1) (a2,b2) : max(a1,a2) < min(b1,b2) = overlap hai a2 se b1 tak
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
