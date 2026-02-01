32192356b46605df396c5eecb938cca6e5122c56
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-02-01 23:05:28 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.22%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array-02-01-2026-23-05-28.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+int binary_search(vector<int>& nums,int s,int e, int target){
+        int mid;
+        while(s<=e){   //eval wo wala case v jb s==e ho
+            mid = s+(e-s)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] >target){
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+        }
+        return -1;
+}
+int get_pivot(vector<int> &nums){  
+    int s=0, e=nums.size()-1;
+    int mid;
+    while(s<e){
+        mid = s+(e-s)/2;
+        if(nums[mid] < nums[e]) //= store ans and goto left
+            e = mid;
+        else
+            s = mid+1;
+    }
+    return s; //index; for pivot elem -> nums[s]  
+}
+    int search(vector<int>& nums, int target) {
+        //find pivot -> smallest element
+        //then, 0 to pivot-1 tk me b.s and pivot to last me b.s
+        long int pivot = get_pivot(nums);  //pivot idx
+
+        long int x = binary_search(nums,0,pivot-1,target); 
+        long int y = binary_search(nums,pivot,nums.size()-1,target);
+  
+        if(x == -1)
+            return y;
+        return x;
+
+    }
+};
