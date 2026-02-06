184fcbc7218505f3c9993206909964aa745c7d1d
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-02-06 14:16:02 +0530

    Time: 22 ms (96.8%), Space: 105.2 MB (14.24%) - LeetHub

---
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array-02-06-2026-14-16-01.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        //max_elem <=k*min_elem
+        // return the minimum number of elements to remove
+        // no of deletn = n-balanced substr
+        int n = nums.size();
+        sort(nums.begin(),nums.end()); 
+        //[1,2,6,9], k=3
+
+        int i=0;  //isse compr
+        int ans = 0; //len of largst balanced arr
+        for(int j=0;j<n;j++){ //i=1, j=1 = ans = 1; i=2,j=1 ; i=4,j=1;
+            if( (long long)(nums[j]) <=  (1LL *k* 1LL *nums[i]))
+                ans = max(ans,j-i+1); //cur ans store and age check
+            else  //i ko badha
+                i++;
+        }
+        return n-ans; //no of deletn
+    }
+};
