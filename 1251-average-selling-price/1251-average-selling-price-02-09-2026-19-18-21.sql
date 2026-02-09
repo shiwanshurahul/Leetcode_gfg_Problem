86d6a9c6d4d60c658e83a91b917fde54def5c72b
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-02-09 19:18:21 +0530

    Time: 845 ms (28%), Space: 0B (100%) - LeetHub

---
+++ 1251-average-selling-price/1251-average-selling-price-02-09-2026-19-18-21.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+#avg selling price of each product = sare product ka  = groupby product
+#and left join kro
+
+-- Prices are valid only within a specific date range, so for each sale, we must pick the correct price based on the date.
+-- so, US.purchase_date BETWEEN P.start_date AND P.end_date v rkho join me
+
+-- If a product has no sales, its average price should be 0.
+
+SELECT P.product_id, IFNULL(ROUND(SUM(P.price * US.units ) / SUM(US.units),2),0) as average_price from Prices as P
+LEFT JOIN UnitsSold as US
+ON P.product_id = US.product_id AND
+US.purchase_date BETWEEN P.start_date AND P.end_date
+GROUP BY P.product_id;
+
