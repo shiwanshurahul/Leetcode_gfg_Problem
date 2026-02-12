5f13bd0a576fedef7850c2337e12191aaadc74c1
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-02-12 14:30:11 +0530

    Time: 12 ms (12.68%), Space: 20.6 MB (12.23%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden-02-12-2026-14-30-10.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+int dp[10001];
+int solve(int i,int n, vector<int>&jumps){
+    if(i>=n)
+        return 0; //add 0 to min step jo lge
+
+    if(dp[i] !=-1)
+        return dp[i];
+
+    int ans = 1e9;
+    for(int j=1;j<=jumps[i];j++){
+        ans = min(ans, 1 + solve(i+j,n,jumps));
+    }
+    return dp[i] = ans;   
+}
+    int minTaps(int n, vector<int>& ranges) {
+        //similar to jump gam 2
+        vector<int> left(n+1,0);  // [0,0,1,2,4,5]
+        vector<int> right(n+1,0); // [3,5,3,4,4,5]
+        vector<int> jumps(n+1, 0);
+
+        for(int i=0;i<n+1;i++){
+            // left[i] = max(0,i-ranges[i]);
+            // right[i] = max(n, min(n,i+ranges[i]));
+            int left_idx = max(0,i-ranges[i]);
+            jumps[left_idx] = max(jumps[left_idx] ,min(n, i+ ranges[i])); 
+        }
+
+        for(int i=0;i<n+1;i++){ 
+            jumps[i] -= i; //converting the rightmost jump indx 
+        }                   //to jump length from i
+        
+        //now same problm
+        memset(dp,-1,sizeof(dp));        
+        int ans = solve(0,n,jumps);
+        return ans == 1e9 ? -1 : ans ;
+    }  //sbka i se initallly call jaega fir sbka j chlega and min aega
+};
