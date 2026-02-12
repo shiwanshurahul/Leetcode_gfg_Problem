a2e484960e9f00bd95151b4902565e3c590bad3e
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-02-12 09:21:00 +0530

    Time: 0 ms (100%), Space: 8.5 MB (6.8%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome-02-12-2026-09-21-00.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        // string is palindrom
+        //non palindrom banao lexiographicaly smalles
+        //gredy
+        //e.g."abccba" -> "aaccba" : replace krdeng 'b' ko 'a' se
+        //= change the first non 'a' character to 'a'
+        //e.g.2. "aaaa" -> "aaab" -> last 'a' ko 'b'
+        //e.g.3 "aabaa" -> "aaaaa" -> still a palindrom
+        //odd len ke middle letter ko change kiya fir v wo palindrom hi rhega
+        //so, "aabab"
+
+        //n/2 len tk me chnage krdo as baki half v same hai
+
+        int n = palindrome.length();
+        if(n==0 || n==1)
+            return "";
+        for(int i=0;i<n/2;i++){
+            if(palindrome[i] !='a'){  //lexicographicly smallest palindrom
+                palindrome[i] = 'a';
+                return palindrome;
+            }    
+        }
+        //agar yaha hai toh sirf 'a' hai s me: "aaaaa"
+        palindrome[n-1] = 'b'; //second smallest 
+        return palindrome;
+    }
+};
