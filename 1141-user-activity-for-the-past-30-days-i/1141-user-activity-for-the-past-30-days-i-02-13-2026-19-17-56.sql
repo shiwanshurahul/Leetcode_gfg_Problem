4d7884ad05c3d8c4ebe9914b9af7520301b635ee
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-02-13 19:17:57 +0530

    Time: 518 ms (26.06%), Space: 0B (100%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i-02-13-2026-19-17-56.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+
+-- 2019-07-27 -> end date
+-- 2019-06-28 -> start date (30 days before)
+-- ek ek krke col deko
+
+-- We need to count how many unique users were active each day within a given date range = groupby date
+
+-- So:
+-- Filter rows between two dates.
+-- For each day, count distinct user_id.
+-- Group results by date.
+
+-- SELECT activity_date as day, count(DISTINCT user_id) 
+-- as active_users from Activity
+-- where activity_date between "2019-06-28" and " 2019-07-27"
+-- group by activity_date;
+
+-- m2:
+SELECT activity_date as day, count(DISTINCT user_id) 
+as active_users from Activity
+where activity_date >= "2019-06-28" and activity_date <= " 2019-07-27"
+group by activity_date;
