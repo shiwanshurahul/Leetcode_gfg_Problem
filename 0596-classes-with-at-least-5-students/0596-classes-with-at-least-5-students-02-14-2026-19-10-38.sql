c503c3145c6574da04268807c0ee97ee34c2827b
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-02-14 19:10:39 +0530

    Time: 317 ms (53.78%), Space: 0B (100%) - LeetHub

---
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students-02-14-2026-19-10-38.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT class from Courses as C GROUP BY C.class HAVING COUNT(C.student) >= 5;
+
+
+#aggregate ke ans pe having lagate, where will raise erro
+
+# m2:
+-- SELECT class from Courses as C HAVING COUNT(C.student) >= 5;
+
+
