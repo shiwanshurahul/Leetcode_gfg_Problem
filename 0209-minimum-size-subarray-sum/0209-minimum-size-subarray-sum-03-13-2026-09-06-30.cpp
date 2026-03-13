e0d3eee5ccb06ba1e2a60cb260e83e78f7352ec5
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-03-13 09:06:30 +0530

    Time: 0 ms (100%), Space: 32 MB (97.49%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum-03-13-2026-09-06-30.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        //min len = min size window nikalo
+        //variable size window -> 2 bar ans cal krne ki kosis
+        int i=0,j=0;
+        int ans=1e9;
+        int sum=0;
+        while(j<nums.size()){
+            sum+=nums[j];
+            if(sum<target){
+                j++;
+            }
+            else if(sum==target){
+                ans = min(ans,j-i+1);
+                j++;
+            }
+            else if(sum>=target){
+                while(sum>=target){
+                    ans = min(ans,j-i+1);    //ans store
+                    sum-= nums[i];          // remove caln for i
+                    i++;                     // slide window  
+                    if(sum==target)
+                        ans = min(ans,j-i+1);
+                }
+                j++;
+            }
+        }
+        return ans == 1e9 ? 0 : ans;
+    }
+};
