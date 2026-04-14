393ccd8c565fbaad2b7d05568c2942c62be4f839
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-04-14 08:17:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors-04-14-2026-08-17-25.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low  = 0, mid=0, high = nums.size()-1;
+        while(mid <=high){
+            if(nums[mid]==0){
+                swap(nums[mid], nums[low]);
+                low++; 
+                mid++;
+            }
+            else if(nums[mid] ==1){
+                mid++;        //mid ko yahi req hai
+            }
+            else{  //nums[mid] == 2 hai
+                swap(nums[mid], nums[high]);
+                high--;
+            } 
+        }
+    }
+};
