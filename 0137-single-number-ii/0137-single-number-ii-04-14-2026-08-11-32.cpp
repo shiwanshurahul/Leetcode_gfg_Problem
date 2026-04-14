28f844711b60939f25d8c761a086af56a5491b10
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-04-14 08:11:30 +0530

    Time: 4 ms (14.37%), Space: 13.4 MB (64.42%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii-04-14-2026-08-11-32.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //svi no ka each digit p set bits(1) check kro
+        // if (nums[i] & (1<<i) !=0 )
+        //     cnt_1++;
+        // for every bit position:
+            //cnt how many numbers have 0
+            //cnt how many numbers have 1
+
+        //Since, most numbers appear 3 times, their bits contribute in 
+        //multiple of 3.
+        // If at any bit position the count of 1s % 3 == 1, it means that 
+        //bit belongs to the unique number. 
+
+        int ans = 0;
+        for(int i=0;i<32;i++){  //check each bit position from 0 to 31.
+            int cnt_1 =0;       //no of set bits at ith positin
+            for(auto it: nums){  //sare no ka ith bit check
+                if((it & (1<<i)) !=0)  //check if ith bit is set
+                    cnt_1++;
+            }
+            //ith bit ka cnt_1 hai apne pas:
+            if(cnt_1 % 3 !=0)    //single no
+                ans = ans | (1<<i);  //us postion pe set bit daldo    
+        }
+        return ans;
+    }
+};
