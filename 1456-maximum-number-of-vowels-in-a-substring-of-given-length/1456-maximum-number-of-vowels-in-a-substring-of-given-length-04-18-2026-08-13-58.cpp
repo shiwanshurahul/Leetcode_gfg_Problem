e8d9a25690da58df22b3acfbd1451f247b9bd306
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-04-18 08:13:58 +0530

    Time: 3 ms (88.92%), Space: 13 MB (90.32%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length-04-18-2026-08-13-58.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+bool isvowel(char ch){
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+        return 1;
+        return 0;
+}
+    int maxVowels(string s, int k) {
+        //fixed le slidin window -> ke pe pahunch ke const rkho
+        int i=0,j=0;
+        int cnt=0;
+        int maxi =0;
+
+        while(j<s.length()){
+            if(isvowel(s[j]))
+                cnt++;
+            if(j-i+1<k){
+                j++;
+            }
+            else if(j-i+1==k){   //window siz maintai rkh
+                maxi = max(cnt,maxi);   //cal ans
+                if(isvowel(s[i]))       //remove cal for i
+                    cnt--;  
+                i++;                    //slide the window
+                j++;
+            }
+        }
+        return maxi;
+    }
+};
