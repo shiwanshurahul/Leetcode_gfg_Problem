a84c4ae5fe6f4262bc0bd72020274d51a8140f84
Shiwanshu . <110823952+shiwanshurahul@users.noreply.github.com>
2026-04-22 08:58:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices-04-22-2026-08-58-14.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        
+        int n = nums.size();
+        int cnt = 0;
+        int ans = 0;
+        for(int i=1;i<n-1;i++){
+            if(nums[i]-nums[i-1] == nums[i+1] - nums[i]){
+                cnt++;      //1, 2, 3
+                ans += cnt; //1, 3, 6
+            }
+            
+        }
+        //last idx ke liye chk
+        return ans;
+    }
+};
